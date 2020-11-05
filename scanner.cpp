e100e15e5e9b90b3fd595eb4e9d84ec63c023300
Peter Sharp <psharp760@gmail.com>
2020-11-04 20:32:19 -0800

    Update scanner.cpp

--- scanner.cpp
+++ scanner.cpp
@@ -12,119 +12,137 @@ using namespace std;
 // WORD DFA 
 // Done by: Howard Tep
 // RE:   (([vowel] n | dwzyj | bmknhpr | t | c | s) 
-bool word (string s)
+bool word(string s)
 {
 
-  int state = 0;
-  int charpos = 0;
+    int state = 0;
+    int charpos = 0;
 
-  // replace the following todo the word dfa(vowel | vowel n | consonant vowel | etc......  **
- 
-     //where vowel = a,e,i,o,e,I, E    refer to Project-partA_w8A10A.doc for RE   
-    while (s[charpos] != '\0') 
+    // replace the following todo the word dfa(vowel | vowel n | consonant vowel | etc......  **
+
+       //where vowel = a,e,i,o,e,I, E    refer to Project-partA_w8A10A.doc for RE   
+    while (s[charpos] != '\0')
     {
-      if (state == 0) {
-        char ch = s[charpos];//storing character
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
-          state = 7;
-        else if (ch == 'd' || ch == 'w' || ch == 'z' || ch == 'y' || ch == 'j')
-          state = 6;
-        else if (ch == 'b' || ch == 'm' || ch == 'k' || ch == 'n' || ch == 'h' || ch == 'p' || ch == 'r' || ch == 'g')
-          state = 5;
-        else if (ch == 't') state = 4;
-        else if (ch == 'c') state = 3;
-        else if (ch == 's') state = 2;
-      }
+        if (state == 0) {
+            char ch = s[charpos];//storing character
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
+                state = 6;
+            else if (ch == 'd' || ch == 'w' || ch == 'z' || ch == 'y' || ch == 'j')
+                state = 5;
+            else if (ch == 'b' || ch == 'm' || ch == 'k' || ch == 'n' || ch == 'h' || ch == 'p' || ch == 'r' || ch == 'g')
+                state = 4;
+            else if (ch == 't') state = 3;
+            else if (ch == 'c') state = 2;
+            else if (ch == 's') state = 1;
+            else return false;
+        }
 
-      else if (state == 1 && s[charpos] == 'n') state = 0;
-      
-      else if (state == 6) {
-        char ch = s[charpos];//storing character
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
-          state = 7;
-      }
+        else if (state == 6) {
+            char ch = s[charpos];//storing character
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
+                state = 6;
+            else if (ch == 'b' || ch == 'm' || ch == 'k' || ch == 'h' || ch == 'p' || ch == 'r' || ch == 'g')
+                state = 4;
+            else if (ch == 's')
+                state = 1;
+            else if (ch == 'c')
+                state = 2;
+            else if (ch == 'h')
+                state = 4;
+            else if (ch == 't')
+                state = 3;
+            else if (ch == 'n')
+                state = 6;
+            else if (ch == 'd' || ch == 'w' || ch == 'z' || ch == 'y' || ch == 'j')
+                state = 5;
+            else return false;
+        }
 
-      else if (state == 5) {
-        char ch = s[charpos];//storing character
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
-          state = 7;
-        else if (ch == 'y')
-          state = 6;
-      }
+        else if (state == 5) {
+            char ch = s[charpos];//storing character
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
+                state = 6;
+            else return false;
+        }
 
-      else if (state == 4) {
-        char ch = s[charpos];//storing character
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
-          state = 7;
-        else if (ch == 's')
-          state = 6;
-      }
+        else if (state == 4) {
+            char ch = s[charpos];//storing character
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
+                state = 6;
+            else if (ch == 's') {
+                state = 5;
+            }
 
-       else if (state == 2) {
-        char ch = s[charpos];//storing character
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
-          state = 7;
-        else if (ch == 'h')
-          state = 6;
-      }
+            else if (ch == 'y') {
+                state = 5;
+            }
+            else return false;
+        }
 
-      else if (state == 3 && s[charpos] == 'h') state = 6;
+        else if (state == 3) {
+            char ch = s[charpos];//storing character
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
+                state = 6;
+            else if (ch == 'y')
+                state = 5;
+            else if (ch == 's') {
+                state = 6;
+            }
+            else return false;
+        }
 
-      else if (state == 7) {
-        char ch = s[charpos];//storing character
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
-          state = 6;
-        
-        else if (ch == 'n') state = 8;
-        else if (ch == 'y') state = 6;
-        else if (ch == 's') state = 2;
-        else if (ch == 'h') state = 5;
-        else if (ch == 'c') state = 3;
-        else if (ch == 't') state = 4;
+        else if (state == 2 && s[charpos] == 'h') {
+            char ch = s[charpos];
+            if (ch == 'h') {
+                state = 5;
+            }
 
-        else if (ch == 'b' || ch == 'm' || ch == 'k' || ch == 'n' || ch == 'h' || ch == 'p' || ch == 'r' || ch == 'g')
-          state = 8;
-        
-        else if (ch == 'd' || ch == 'w' || ch == 'z' || ch == 'y' || ch == 'j')
-          state = 6;
-      }
+            else return false;
+        }
 
-      else if (state == 8) {
-        char ch = s[charpos];//storing character
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
-          state = 7;
-        
-        else if (ch == 'n') state = 5;
-        else if (ch == 'y') state = 6;
-        else if (ch == 's') state = 2;
-        else if (ch == 'h') state = 5;
-        else if (ch == 'c') state = 3;
-        else if (ch == 't') state = 4;
+        else if (state == 1) {
+            char ch = s[charpos];
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
+                state = 6;
+            else if (ch == 'h')
+                state = 5;
+            else return false;
+        }
 
-        else if (ch == 'b' || ch == 'm' || ch == 'k' || ch == 'n' || ch == 'h' || ch == 'p' || ch == 'r' || ch == 'g')
-          state = 5;
-        
-        else if (ch == 'd' || ch == 'w' || ch == 'z' || ch == 'y' || ch == 'j')
-          state = 6;
-      }
+        else if (state == 7) {
+            char ch = s[charpos];//storing character
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'I' || ch == 'E')
+                state = 6;
 
-      else return(false);
-      charpos++;
+            else if (ch == 's') state = 1;
+            else if (ch == 'c') state = 2;
+            else if (ch == 't') state = 3;
+
+            else if (ch == 'b' || ch == 'm' || ch == 'k' || ch == 'n' || ch == 'h' || ch == 'p' || ch == 'r' || ch == 'g')
+                state = 4;
+
+            else if (ch == 'd' || ch == 'w' || ch == 'z' || ch == 'y' || ch == 'j')
+                state = 5;
+            else return false;
+        }
+
+        charpos++;
     }//end of while
 
     // where did I end up????
-    if (state == 0 || state == 7 || state == 8) return(true);  // end in a final state
+    if (state == 0 || state == 7 || state == 6) return(true);  // end in a final state
     else return(false);
 
 }
 
 // PERIOD DFA 
 // Done by: Peter Sharp
-bool period (string s)
-{  
+bool period(string s)
+{
     if (s[0] == '.') {
         return true;
-    } else {
+    }
+    else {
         return false;
     }
 }
@@ -136,7 +154,7 @@ bool period (string s)
 // ** Update the tokentype to be WORD1, WORD2, PERIOD, ERROR, EOFM, etc.
 enum tokentype { WORD1, WORD2, PERIOD, VERB, VERBNEG, VERBPAST, VERBPASTNEG, IS, WAS, OBJECT, SUBJECT, DESTINATION, PRONOUN, CONNECTOR, EOFM, ERROR };
 // ** For the display names of tokens - must be in the same order as the tokentype.
-string tokenName[30] = {"WORD1", "WORD2", "PERIOD", "VERB", "VERBNEG", "VERBPAST", "VERBPASTNEG", "IS", "WAS", "OBJECT", "SUBJECT", "DESTINATION", "PRONOUN", "CONNECTOR", "EOFM", "ERROR"};
+string tokenName[30] = { "WORD1", "WORD2", "PERIOD", "VERB", "VERBNEG", "VERBPAST", "VERBPASTNEG", "IS", "WAS", "OBJECT", "SUBJECT", "DESTINATION", "PRONOUN", "CONNECTOR", "EOFM", "ERROR" };
 
 // ** Need the reservedwords table to be set up here. 
 // ** Do not require any file input for this. Hard code the table.
@@ -174,68 +192,87 @@ int scanner(tokentype& tt, string& w)
 {
     fin >> w;
 
-    if (word(w)) {
+    //checking to see if we have a period first
+    //checking to see if word is period first, since it is th easiest thing to check for
+    if (period(w)) {
+        tt = PERIOD;
+    }
+
+    else if (word(w)) {
         bool reserved = false;
         for (int i = 0; i < 20; i++) {
             if (w == reservedwords[i][0]) {
                 if (reservedwords[i][1] == "VERB") {
-		    tt = VERB;
-		} else if (reservedwords[i][1] == "VERBNEG") {
-  		    tt = VERBNEG;
-	        } else if (reservedwords[i][1] == "VERBPASTNEG") {
-		    tt = VERBPASTNEG;
-                } else if (reservedwords[i][1] == "IS") {
- 		    tt = IS;
-		} else if (reservedwords[i][1] == "WAS") {
-		    tt = WAS;
-		} else if (reservedwords[i][1] == "OBJECT") {
-		    tt = OBJECT;
-		} else if (reservedwords[i][1] == "SUBJECT") {
-		    tt = SUBJECT;
-		} else if (reservedwords[i][1] == "DESTINATION") {
-		    tt = DESTINATION;
-		} else if (reservedwords[i][1] == "PRONOUN") {
-		    tt = PRONOUN;
-		} else if (reservedwords[i][1] == "CONNECTOR") {
-		    tt = CONNECTOR;
-		} else if (reservedwords[i][1] == "EOFM") {
-		    tt = EOFM;
-		}
-		reserved = true;
-		break;
+                    tt = VERB;
+                }
+                else if (reservedwords[i][1] == "VERBNEG") {
+                    tt = VERBNEG;
+                }
+                else if (reservedwords[i][1] == "VERBPAST") {
+                    tt = VERBPAST;
+                }
+                else if (reservedwords[i][1] == "VERBPASTNEG") {
+                    tt = VERBPASTNEG;
+                }
+                else if (reservedwords[i][1] == "IS") {
+                    tt = IS;
+                }
+                else if (reservedwords[i][1] == "WAS") {
+                    tt = WAS;
+                }
+                else if (reservedwords[i][1] == "OBJECT") {
+                    tt = OBJECT;
+                }
+                else if (reservedwords[i][1] == "SUBJECT") {
+                    tt = SUBJECT;
+                }
+                else if (reservedwords[i][1] == "DESTINATION") {
+                    tt = DESTINATION;
+                }
+                else if (reservedwords[i][1] == "PRONOUN") {
+                    tt = PRONOUN;
+                }
+                else if (reservedwords[i][1] == "CONNECTOR") {
+                    tt = CONNECTOR;
+                }
+                else if (reservedwords[i][1] == "EOFM") {
+                    tt = EOFM;
+                }
+                reserved = true;
+                break;
             }
         }
-	if (!reserved) {
-	    if (w[w.length() - 1] == 'I' || w[w.length() - 1] == 'E') {
-	        tt = WORD2;
-	    } else {
-		tt = WORD1;
-	    }
-	}
+        if (!reserved) {
+            if (w[w.length() - 1] == 'I' || w[w.length() - 1] == 'E') {
+                tt = WORD2;
+            }
+            else {
+                tt = WORD1;
+            }
+        }
     }
-    else if (period(w)) {
-        tt = PERIOD;
-    } else if (w == "eofm") {
+    else if (w == "eofm") {
         tt = EOFM;
-    } else {
+    }
+    else {
         cout << "LEXICAL ERROR: " << w << " is not a valid token.\n";
         tt = ERROR;
     }
     return 0;
 
-  // ** Grab the next word from the file via fin
-  // 1. If it is eofm, return right now.   
-  /*  **
-  2. Call the token functions (word and period) 
-     one after another (if-then-else).
-     Generate a lexical error message if both DFAs failed.
-     Let the tokentype be ERROR in that case.
-  3. If it was a word,
-     check against the reservedwords list.
-     If not reserved, tokentype is WORD1 or WORD2
-     decided based on the last character.
-  4. Return the token type & string  (pass by reference)
-  */
+    // ** Grab the next word from the file via fin
+    // 1. If it is eofm, return right now.   
+    /*  **
+    2. Call the token functions (word and period)
+       one after another (if-then-else).
+       Generate a lexical error message if both DFAs failed.
+       Let the tokentype be ERROR in that case.
+    3. If it was a word,
+       check against the reservedwords list.
+       If not reserved, tokentype is WORD1 or WORD2
+       decided based on the last character.
+    4. Return the token type & string  (pass by reference)
+    */
 }//the end of scanner
 
 // The temporary test driver to just call the scanner repeatedly  
@@ -244,28 +281,27 @@ int scanner(tokentype& tt, string& w)
 // Done by:  Louis
 int main()
 {
-  tokentype thetype;
-  string theword; 
-  string filename;
+    tokentype thetype;
+    string theword;
+    string filename;
 
-  cout << "Enter the input file name: ";
-  cin >> filename;
+    cout << "Enter the input file name: ";
+    cin >> filename;
 
-  fin.open(filename.c_str());
+    fin.open(filename.c_str());
 
-  // the loop continues until eofm is returned.
-   while (true)
+    // the loop continues until eofm is returned.
+    while (true)
     {
-       scanner(thetype, theword);  // call the scanner which sets
-                                   // the arguments  
-       if (theword == "eofm") break;  // stop now
+        scanner(thetype, theword);  // call the scanner which sets
+                                    // the arguments  
+        if (theword == "eofm") break;  // stop now
 
-       cout << "Type is:" << tokenName[thetype] << endl;
-       cout << "Word is:" << theword << endl;   
+        cout << "Type is:" << tokenName[thetype] << endl;
+        cout << "Word is:" << theword << endl;
     }
 
-   cout << "End of file is encountered." << endl;
-   fin.close();
+    cout << "End of file is encountered." << endl;
+    fin.close();
 
 }// end
-
